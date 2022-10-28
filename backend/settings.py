1d03282d3aa62463b7cf5907740ceb20f8eb0a6a
seyiadel <seyiadel03@gmail.com>
2022-10-28 15:11:41 +0100

    changed allowed hosts to accept all origins

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['seyiadel-hng9-stage1.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
